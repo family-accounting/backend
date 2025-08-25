2d422833aa8b8e7a4a46df885f46acc986d5ce2d
Amir Maghami <Maghami.A1987@gmail.com>
2025-08-25 16:10:26 +0330

    add notifications

---
+++ src/notifications/notifications.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import type { CreateNotificationDto, UpdateNotificationDto } from './dto/notification.dto';
+
+@Injectable()
+export class NotificationsService {
+  create(createNotificationDto: CreateNotificationDto) {
+    return 'This action adds a new notification';
+  }
+
+  findAll() {
+    return `This action returns all notifications`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} notification`;
+  }
+
+  update(id: number, updateNotificationDto: UpdateNotificationDto) {
+    return `This action updates a #${id} notification`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} notification`;
+  }
+}
