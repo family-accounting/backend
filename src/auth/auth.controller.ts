7199dcd2a6be4ad8b1766d9a508376242722382b
Amir Maghami <Maghami.A1987@gmail.com>
2025-08-26 23:36:20 +0200

    add bcrypt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,25 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { loginAuthSchema, type LoginAuthDto, type RegisterAuthDto } from './dto/auth.dto';
+import { loginAuthSchema, registerAuthSchema, type LoginAuthDto, type RegisterAuthDto } from './dto/auth.dto';
 import { ZodValidationPipe } from 'src/common/pipe/zod-validation.pipe';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+    
+  ) { }
 
   @Post('login')
   login(@Body(new ZodValidationPipe(loginAuthSchema)) dto: LoginAuthDto) {
     return this.authService.login(dto);
   }
 
-  
+
   @Post('register')
-  register(@Body() dto: RegisterAuthDto) {
+  async register(@Body(new ZodValidationPipe(registerAuthSchema)) dto: RegisterAuthDto) {
     return this.authService.register(dto);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,10 @@ import type { LoginAuthDto, RegisterAuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { I18nContext, I18nService } from 'nestjs-i18n';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
+import { pick } from 'lodash';
+import { UserDto } from 'src/users/dto/user.dto';
 
 
 @Injectable()
@@ -10,7 +14,8 @@ export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
-    private readonly i18n: I18nService
+    private readonly i18n: I18nService,
+    private readonly configService: ConfigService,
   ) { }
 
   async login(dto: LoginAuthDto) {
@@ -21,18 +26,29 @@ export class AuthService {
       throw new UnauthorizedException(message);
     }
     const payload = { mobile: dto.mobile, sub: user.id };
+    const access_token = this.jwtService.sign(payload);
+    const refresh_token = this.jwtService.sign(payload, { expiresIn: '7d' });
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token,
+      user: pick(user, ['id', 'username', 'mobile', 'firstName', 'lastName', 'createdAt', 'updatedAt', 'permissions']),
+      refresh_token,
     };
   }
 
   async register(dto: RegisterAuthDto) {
-    return this.usersService.create(dto);
+    const user = await this.usersService.findByMobile(dto.mobile);
+    if (user) {
+      const message = await this.i18n.t('auth.register.error');
+      throw new UnauthorizedException(message);
+    }
+    const saltRounds = this.configService.get<number>('BCRYPT_SALT_ROUNDS')!;
+    const hashedPassword = await bcrypt.hash(dto.password, saltRounds);
+    return await this.usersService.create({ ...dto, password: hashedPassword });
   }
 
   async validate(mobile: string, password: string) {
     const user = await this.usersService.findByMobile(mobile);
-    if (user && user.password === password) {
+    if (user && await bcrypt.compare(password, user.password)) {
       return user;
     }
     return null;
