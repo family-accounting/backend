5c08dd3a9c3b64ecca998c150292b9febfd61f97
Amir Maghami <Maghami.A1987@gmail.com>
2025-09-30 10:08:01 +0330

    Add PermissionEntity with TypeORM integration
    Introduces a new PermissionEntity with fields for id, name, description, timestamps, and a many-to-many relation to roles. Updates the permissions module to register PermissionEntity with TypeOrmModule for database integration.

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,1 +1,26 @@
-export class Permission {}
+import { Column, CreateDateColumn, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import type {Id, CreatedAt ,UpdatedAt} from "@/common/types";
+import { Entity, BaseEntity } from "typeorm";
+import { RoleEntity } from "@/roles/entities/role.entity";
+
+@Entity({ name: 'permissions' })
+export class PermissionEntity extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: Id;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name: string;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @CreateDateColumn({ type: 'timestamp', nullable: false })
+    createdAt: CreatedAt;   
+
+    @UpdateDateColumn({ type: 'timestamp', nullable: false })
+    updatedAt: UpdatedAt;
+
+    // relation with role
+    @ManyToMany(() => RoleEntity, (role) => role.permissions)
+    roles: RoleEntity[];
+}
--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
+import { PermissionEntity } from './entities/permission.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PermissionEntity])],
   controllers: [PermissionsController],
   providers: [PermissionsService],
 })
