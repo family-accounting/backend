1b35028de40d59453b40449d87a9f2a5c530d2e0
Amir Maghami <Maghami.A1987@gmail.com>
2025-09-30 10:08:14 +0330

    Add RoleEntity with permissions and TypeORM integration
    Introduces the RoleEntity class with fields for id, name, description, timestamps, and a many-to-many relation to permissions. Updates RolesModule to import TypeOrmModule with RoleEntity for database integration.

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,25 @@
-export class Role {}
+import type { CreatedAt, Id, UpdatedAt } from "@/common/types";
+import { PermissionEntity } from "@/permissions/entities/permission.entity";
+import { BaseEntity, Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'roles' })
+export class RoleEntity extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: Id;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name: string;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @CreateDateColumn({ type: 'timestamp', nullable: false })
+    createdAt: CreatedAt;
+
+    @UpdateDateColumn({ type: 'timestamp', nullable: false })
+    updatedAt: UpdatedAt;
+
+    // relation with permission
+    @ManyToMany(() => PermissionEntity, (permission) => permission.roles)
+    permissions: PermissionEntity[];
+}
