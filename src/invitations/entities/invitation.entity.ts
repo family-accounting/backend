2d3525f334de11f221a552157532504f168c73ee
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-01 11:18:13 +0330

    Define Invitation entity with TypeORM decorators
    Expanded the Invitation class to include id, email, status, createdAt, and updatedAt fields using TypeORM decorators. This sets up the entity for database integration and enforces schema constraints.

--- src/invitations/entities/invitation.entity.ts
+++ src/invitations/entities/invitation.entity.ts
@@ -1,1 +1,21 @@
-export class Invitation {}
+import type{ CreatedAt, Id, UpdatedAt } from "@/common/types";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: 'invitations' })
+export class Invitation {
+    @PrimaryGeneratedColumn('uuid')
+    id: Id;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    status: '';
+
+    @CreateDateColumn({ type: 'timestamp', nullable: false })
+    createdAt: CreatedAt;
+
+    @UpdateDateColumn({ type: 'timestamp', nullable: false })
+    updatedAt: UpdatedAt;
+
+}
